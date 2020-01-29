076c982f0ee27f4b63cfea96b55a092e0fc5d84a
UmarAkhtar <44348481+UmarAkhtar@users.noreply.github.com>
2020-01-29 17:28:58 +0000

    Added all files necessary

---
+++ GameTechProject/InputManager.cpp
@@ -0,0 +1,23 @@
+
+#include "InputManager.h"
+
+//class InputManager {
+//	void InputManager::update() {
+//		if (keys[SDL_SCANCODE_W]) eye = moveForward(eye, r, 0.1f);
+//		if (keys[SDL_SCANCODE_S]) eye = moveForward(eye, r, -0.1f);
+//		if (keys[SDL_SCANCODE_A]) eye = moveRight(eye, r, -0.1f);
+//		if (keys[SDL_SCANCODE_D]) eye = moveRight(eye, r, 0.1f);
+//		if (keys[SDL_SCANCODE_R]) eye.y += 0.1;
+//		if (keys[SDL_SCANCODE_F]) eye.y -= 0.1;
+//
+//		if (keys[SDL_SCANCODE_I]) lightPos[2] -= 0.1;
+//		if (keys[SDL_SCANCODE_J]) lightPos[0] -= 0.1;
+//		if (keys[SDL_SCANCODE_K]) lightPos[2] += 0.1;
+//		if (keys[SDL_SCANCODE_L]) lightPos[0] += 0.1;
+//		if (keys[SDL_SCANCODE_U]) lightPos[1] += 0.1;
+//		if (keys[SDL_SCANCODE_H]) lightPos[1] -= 0.1;
+//
+//		if (keys[SDL_SCANCODE_COMMA]) r -= 1.0f;
+//		if (keys[SDL_SCANCODE_PERIOD]) r += 1.0f;
+//	}
+//};
