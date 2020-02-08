ff3377dd4862c9fba1cb7dad03fe68ff49f47bcb
kmce25 <31890174+kmce25@users.noreply.github.com>
2020-02-08 15:17:36 +0000

    sceneManager added

---
+++ GameTechProject/sceneManager.cpp
@@ -0,0 +1,69 @@
+#include "sceneManager.h"
+
+void sceneManager::glewInitilisation()
+{
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{ 
+		std::cout << "glewInit failed, aborting." << endl;
+		exit(1);
+	}
+	cout << glGetString(GL_VERSION) << endl;
+}
+
+void sceneManager::loadModel()
+{
+}
+
+void sceneManager::loadShader()
+{
+}
+
+void sceneManager::update()
+{
+}
+
+sceneManager::sceneManager(int windowWidth, int windowHeight) : windowWidth(windowWidth), windowHeight(windowHeight)
+{
+	window = setupRC(context);
+	glewInitilisation();
+	loadShader();
+	loadModel();
+	draw();
+}
+
+sceneManager::~sceneManager()
+{
+
+}
+
+SDL_Window* sceneManager::setupRC(SDL_GLContext& context)
+{
+	SDL_Window* window;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		cout << "Unable to initialize SDL " << endl;;
+
+	// Request an OpenGL 3.0 context.
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);  // double buffering on
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8); // 8 bit alpha buffering
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4); // Turn on x4 multisampling anti-aliasing (MSAA)
+
+	// Create 800x600 window
+	window = SDL_CreateWindow("SDL/GLM/OpenGL Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		windowWidth, windowHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+	if (!window) // Check window was created OK
+		cout << "Unable to create window" << endl;
+
+	context = SDL_GL_CreateContext(window); // Create opengl context and attach to window
+	SDL_GL_SetSwapInterval(1); // set swap buffers to sync with monitor's vertical refresh rate
+	return window;
+}
+
+
---
+++ GameTechProject/sceneManager.h
@@ -0,0 +1,40 @@
+#ifndef SCENE_MANAGER_H
+#define SCENE_MANAGER_H
+#pragma once
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <SDL.h>
+#include <glew.h>
+
+
+using namespace std;
+
+class sceneManager
+{
+private:
+	void glewInitilisation();
+	void loadModel();
+	void loadShader();
+	void update();
+
+	int windowWidth;
+	int windowHeight;
+
+
+public:
+	sceneManager(int windowWidth, int windowHeight);// constructor	
+	~sceneManager();
+
+	void draw();
+
+	SDL_GLContext context;
+	SDL_Window* window;
+	SDL_Window* setupRC(SDL_GLContext& context);
+
+	shared_ptr<Shader> ourShader;
+	shared_ptr<Model> ourModel;
+
+};
+
+#endif // !SCENE_MANAGER
